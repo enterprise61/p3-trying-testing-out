571d4266b019d3609d9e884d808a1fdae3f6302d
enterprise61 <buford.poindexter@gmail.com>
2026-04-17 12:37:39 -0500

    Refactor App component to use default export

--- .p3-trying-testing-out/src/App.jsx
+++ .p3-trying-testing-out/src/App.jsx
@@ -1,15 +1,8 @@
 
 import './App.css'
 
-function App() {
-
-
-  return (
-    <>
-     <h1>Open for business</h1>
+export default function App() {
+  return <div>{<h1>Open for business</h1>
      <button>Click for After-Hours</button>
-    </>
-  )
-}
-
-export default App
+    } </div>;
+} 
